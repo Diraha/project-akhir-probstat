16afa7a2862b90172321503a44a55757aaeb996d
Limamaungoding <shirasalimaf@gmail.com>
2026-06-02 13:29:34 +0700

    nambah 1.3.2

--- probstat/script/Project-Akhir-Probstat.R
+++ probstat/script/Project-Akhir-Probstat.R
@@ -14,3 +14,29 @@ cat("Jumlah Observasi (Baris) :", dimensi[1])
 cat("Jumlah Variabel (Kolom)  :", dimensi[2])
 
 # 1.3.2 Statistika Deskriptif
+# Mengambil variabel numerik
+data_numerik <- data_pembangunan[sapply(data_pembangunan, is.numeric)]
+
+# Ringkasan statistik deskriptif
+summary(data_numerik)
+
+# Mean
+sapply(data_numerik, mean, na.rm = TRUE)
+
+# Median
+sapply(data_numerik, median, na.rm = TRUE)
+
+# Nilai minimum
+sapply(data_numerik, min, na.rm = TRUE)
+
+# Nilai maksimum
+sapply(data_numerik, max, na.rm = TRUE)
+
+# Standar deviasi
+sapply(data_numerik, sd, na.rm = TRUE)
+
+# Varians
+sapply(data_numerik, var, na.rm = TRUE)
+
+# Kuartil
+sapply(data_numerik, quantile, na.rm = TRUE)
